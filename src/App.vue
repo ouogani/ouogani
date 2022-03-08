c41e04b399607cd45f7ebb6e2781644aa06ee60a
ouogani <ouogani@gmail.com>
2022-03-09 05:44:20 +0900

    emptypage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
 <div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+
 </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
